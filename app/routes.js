7ea94141faa3103dbdc6f2fca149e534928aa963
Aniruddh Agarwal <aaniruddh99@gmail.com>
2015-12-15 23:12:25 +0800

    Add datix

--- app/routes.js
+++ app/routes.js
@@ -1,57 +1,27 @@
-var Todo = require('./models/todo');
-
-function getTodos(res){
-	Todo.find(function(err, todos) {
-
-			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-			if (err)
-				res.send(err)
-
-			res.json(todos); // return all todos in JSON format
-		});
-};
+var moment = require('moment');
 
 module.exports = function(app) {
+	app.get('/api/dates/:date', function(req, res) {
+		var date = {};
 
-	// api ---------------------------------------------------------------------
-	// get all todos
-	app.get('/api/todos', function(req, res) {
+		if (isNaN(req.params.date)) {
+			date.unix = moment(req.params.date).unix();
+			if(isNaN(date.unix)) {
+				date.unix = null;
+				date.dateStr = null;
+			} else {
+				date.dateStr = req.params.date;
+			}
 
-		// use mongoose to get all todos in the database
-		getTodos(res);
+		} else {
+			date.unix = Number(req.params.date);
+			date.dateStr = moment.unix(Number(request.params.date)).format("MMMM Do YYYY, h:mm:ss a");
+		}
+
+		res.json(date);
 	});
 
-	// create todo and send back all todos after creation
-	app.post('/api/todos', function(req, res) {
-
-		// create a todo, information comes from AJAX request from Angular
-		Todo.create({
-			text : req.body.text,
-			done : false
-		}, function(err, todo) {
-			if (err)
-				res.send(err);
-
-			// get and return all the todos after you create another
-			getTodos(res);
-		});
-
-	});
-
-	// delete a todo
-	app.delete('/api/todos/:todo_id', function(req, res) {
-		Todo.remove({
-			_id : req.params.todo_id
-		}, function(err, todo) {
-			if (err)
-				res.send(err);
-
-			getTodos(res);
-		});
-	});
-
-	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile('./public/index.html');
 	});
 };
--- public/js/core.js
+++ public/js/core.js
@@ -1,1 +1,24 @@
-angular.module('scotchTodo', ['todoController', 'todoService']);
+var myApp = angular.module('datixApp', []);
+
+myApp
+	.controller('mainCtr', function($scope) {
+		$scope.$watch('date', function(newValue, oldValue) {
+	    if(newValue !== undefined) {
+	      if (isNaN(newValue)) {
+	        $scope.unix = moment(newValue).unix();
+	        if(isNaN($scope.unix)) {
+	          $scope.unix = 'null';
+	          $scope.dateStr = 'null';
+	        } else {
+	          $scope.dateStr = "'" + newValue + "'";
+	        }
+	      } else {
+	        $scope.unix = newValue;
+	        $scope.dateStr = moment.unix(Number(newValue)).format("MMMM Do YYYY, h:mm:ss a");
+	      }
+	    } else {
+	      $scope.unix = 'null';
+	      $scope.dateStr = 'null';
+	    }
+	  });
+	});
